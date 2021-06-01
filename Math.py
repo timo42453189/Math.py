82127e9ed04cc543e4cb7d9e6ce1af250b934a18
timo42453189 <66437994+timo42453189@users.noreply.github.com>
2021-06-01 13:37:58 +0200

    Create Math.py

---
+++ Math.py
@@ -0,0 +1,19 @@
+class Mathe():
+
+    def zähler(zähler):
+        zähler = zähler + 1
+        return zähler
+
+
+    def bedingungen(zähler):
+        if zähler / 4:
+            if zähler / 9:
+                if zähler / 2:
+                    print(zähler)
+
+zähler = 1
+#Mathe.zähler(zähler)
+#Mathe.bedingungen(zähler)
+
+zähler = zähler / 11
+print(zähler)
